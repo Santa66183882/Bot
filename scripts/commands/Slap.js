80c05e95005b657e63c7a6319b717a3c4740ea4e
Trisha529 <166020668+Trisha529@users.noreply.github.com>
2024-04-04 19:13:35 +0600

    Create Slap.js

---
+++ scripts/commands/Slap.js
@@ -0,0 +1,57 @@
+/**
+* @author ProCoderMew
+* @warn Do not edit code or edit credits
+*/
+
+module.exports.config = {
+    name: "slap",
+    version: "2.0.0",
+    permssion: 2,
+    credits: "Mohammad Nayan",
+    description: "",
+    prefix: true,
+    category: "fuck",
+    usages: "[tag]",
+    cooldowns: 5,
+    dependencies: {
+        "axios": "",
+        "fs-extra": "",
+        "path": "",
+        "jimp": ""
+    }
+};
+
+
+module.exports.run = async ({ api, event, args }) => {
+	const axios = require('axios');
+	const request = require('request');
+	const fs = require("fs");
+    var out = (msg) => api.sendMessage(msg, event.threadID, event.messageID);
+  if (!args.join("")) return out("Please tag someone");
+  else
+  return axios.get('https://api.satou-chan.xyz/api/endpoint/slap').then(res => {
+        let getURL = res.data.url;
+        let ext = getURL.substring(getURL.lastIndexOf(".") + 1);
+        var mention = Object.keys(event.mentions)[0];
+                  let tag = event.mentions[mention].replace("@", "");    
+        
+ let callback = function () {
+            api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+        api.sendMessage({
+						        body: "Slapped! " + tag + "\n\n*sorry, i thought there's mosquito*",
+                                          mentions: [{
+          tag: tag,
+          id: Object.keys(event.mentions)[0]
+        }],
+						attachment: fs.createReadStream(__dirname + `/cache/slap.${ext}`)
+					}, event.threadID, () => fs.unlinkSync(__dirname + `/cache/slap.${ext}`), event.messageID)
+				};
+ //   }
+        request(getURL).pipe(fs.createWriteStream(__dirname + `/cache/slap.${ext}`)).on("close", callback);
+			})
+    .catch(err => {
+                     api.sendMessage("Failed to generate gif, be sure that you've tag someone!", event.threadID, event.messageID);
+    api.setMessageReaction("☹️", event.messageID, (err) => {}, true);
+                  })     
+                                                                        }
+                                                                        
